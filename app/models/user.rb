5999caa8ab591b594051404e4734a307c8038ebd
PhilLeeChin <phil.leechin@outlook.com>
2020-08-01 21:06:16 -0400

    add validate for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :username, presence: true
+  validates :username, uniqueness: true
+
   has_many :computers
   has_many :games, through: :computers
 end
