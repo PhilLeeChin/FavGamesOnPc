66c6cc2283d1fe2b642f3441df9fcc6b338d0684
PhilLeeChin <phil.leechin@outlook.com>
2020-08-01 21:07:59 -0400

    add controller direction for devise_for

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  
+  
   resources :computers
   resources :games
-  devise_for :users
-  
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :users, only: [:show]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
