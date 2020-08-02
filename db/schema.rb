921b37346b5bc5dae5b9b307acf24db79772d8f9
PhilLeeChin <phil.leechin@outlook.com>
2020-08-01 21:07:16 -0400

    re-migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_01_180454) do
+ActiveRecord::Schema.define(version: 2020_08_01_181803) do
+
+  create_table "computers", force: :cascade do |t|
+    t.string "name"
+    t.string "system"
+    t.integer "user_id", null: false
+    t.integer "game_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_id"], name: "index_computers_on_game_id"
+    t.index ["user_id"], name: "index_computers_on_user_id"
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.string "game_client"
+    t.text "tags"
+    t.text "genres"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,8 +41,11 @@ ActiveRecord::Schema.define(version: 2020_08_01_180454) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "computers", "games"
+  add_foreign_key "computers", "users"
 end
